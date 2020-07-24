d9a8fe940d7ce11f391de87f30c4394958cc9268
brunoibias <brunoibias@gmail.com>
2020-07-24 18:35:32 -0300

    renderizando lista de array

--- previsao_do_tempo/src/App.js
+++ previsao_do_tempo/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-export default class App extends React.Component{
-  render(){
-    return <h1>Hello Word!!!</h1>
+// rnderizando lista de array
+export default class App extends React.Component {
+  render() {
+    const jedi = [
+      "Obi Wan Kenobi",
+      "Luke Skywalker",
+      "YODA",
+      "Mace Windu"
+    ]
+
+    return (
+      <div>
+        <h1>Jedis</h1>
+        <ul>{
+          jedi.map((jedi, index) => { //O index esta funcionando como um id
+            return <li key={index}>{jedi}</li>
+          })
+        }</ul>
+      </div>
+    );
   }
 }
+
