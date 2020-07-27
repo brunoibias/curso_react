21bdb522cd991ec854669f000f8c49ec028060a1
brunoibias <brunoibias@gmail.com>
2020-07-27 19:30:05 -0300

    Uttilizando API e estilizando a tabela / final

--- previsao_do_tempo/src/App.js
+++ previsao_do_tempo/src/App.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import axios from 'axios';
+
+const API = 'https://api.hgbrasil.com/weather?woeid=455861&format=json-cors&locale=pt';
+
+// utilizando uma API
+export default class App extends React.Component {
+
+  state = {
+    city: '',
+    forecast: [],
+    results: {}
+  };
+
+  componentDidMount() {
+    axios.get(API)
+      .then(({ data }) => {
+        console.log('aqui', data)
+        this.setState({
+          city: data.results.city_name,
+          forecast: data.results.forecast,
+          results: data.results
+        })
+      });
+  }
+  render() {
+    return (
+      <div className='container z-depth-4'>
+        <h1><b>&nbsp;{this.state.city}</b></h1>
+        <table className="striped centered">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Min.</th>
+              <th>Max.</th>
+              <th>Velocidade do vento</th>
+              <th>Condição</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>{
+            this.state.forecast.map((day, index) => {
+              return (
+                <tr key={index}>
+                  <td>{day.date}</td>
+                  <td>{day.min}º</td>
+                  <td>{day.max}º</td>
+                  <td>{this.state.results.wind_speedy}</td>
+                  <td>{day.description}</td>
+                  <td><img width="70px" src={`/weather-icons/${day.condition}.svg`} alt='day.description'/></td>
+                </tr>
+              )
+            })
+          }
+
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 // rnderizando lista de array
-export default class App extends React.Component {
+/* export default class App extends React.Component {
   render() {
     const jedi = [
       "Obi Wan Kenobi",
@@ -21,5 +81,5 @@ export default class App extends React.Component {
       </div>
     );
   }
-}
+} */
 
