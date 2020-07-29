e81930eb240e030b7e8d86e0b3f8f9b9353b026b
brunoibias <brunoibias@gmail.com>
2020-07-29 16:31:40 -0300

    FINAL

---
+++ todo_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TodoApp extends React.Component {
+  render() {
+    return (
+      <div className='container z-depth-4'>
+        <input />
+        <button> Adicionar </button>
+      </div>
+    );
+  }
+}
